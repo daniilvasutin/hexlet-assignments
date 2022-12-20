f23f43da33fc33897352ac85d2fff94c63c004f5
@hexlet/cli <support@hexlet.io>
2022-12-20 19:20:33 +0700

    submit java-collections-ru/lists

---
+++ java-collections-ru/lists/src/main/java/exercise/App.java
@@ -0,0 +1,31 @@
+package exercise;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+// BEGIN
+class App {
+    public static void main(String[] args) {
+        //System.out.println("Hello, Hexlet!");
+    }
+
+    public static boolean scrabble(String symbols, String word) {
+
+        int length = word.length();
+        String[] letters = symbols.split("");
+        ArrayList coll = new ArrayList(Arrays.asList(letters));
+
+        for (int i = 0; i < length; i++) {
+            String current = word.substring(i, i + 1).toLowerCase();
+
+            if (!coll.contains(current)) {
+                return false;
+            }
+
+            coll.remove(current);
+        }
+
+        return true;
+    }
+}
+//END
