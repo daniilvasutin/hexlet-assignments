5b5b3917f33ee76a814192e54079fe00eff35c92
@hexlet/cli <support@hexlet.io>
2022-12-29 18:38:56 +0700

    submit java-collections-ru/generics

--- java-collections-ru/generics/src/main/java/exercise/App.java
+++ java-collections-ru/generics/src/main/java/exercise/App.java
@@ -1,6 +1,8 @@
 package exercise;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 // BEGIN
 public class App {
@@ -8,13 +10,13 @@ public class App {
 
     }
 
-    public static List findWhere(List<Map<String, String>> books, Map<String, String> dictionary) {
+    public static List<Map<String, String>> findWhere(List<Map<String, String>> books, Map<String, String> dictionary) {
 
         List<Map<String, String>> foundBooks = new ArrayList<>();
 
         for (Map<String, String> book : books) {
             boolean check = false;
-            for (Map.Entry where : dictionary.entrySet()) {
+            for (Map.Entry<String, String> where : dictionary.entrySet()) {
                 if (book.containsValue(where.getValue())) {
                     if (book.containsKey(where.getKey())) {
                         check = true;
@@ -27,7 +29,9 @@ public class App {
                     break;
                 }
             }
-            if (check) foundBooks.add(book);
+            if (check) {
+                foundBooks.add(book);
+            }
         }
 
         return foundBooks;
